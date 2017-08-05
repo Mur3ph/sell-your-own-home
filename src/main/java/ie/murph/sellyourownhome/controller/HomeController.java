cc37974700ca8fc24fd6fdbdb47df4dca6a385b5
Mur3ph <paulmurphymainstreet@gmail.com>
2017-08-05 22:56:38 +0100

    added get post

--- src/main/java/ie/murph/sellyourownhome/controller/HomeController.java
+++ src/main/java/ie/murph/sellyourownhome/controller/HomeController.java
@@ -7,25 +7,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-	
-//	http://localhost:8080/home/index
-	@RequestMapping(value="/home/index")
-    public String index(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) 
-    {
+
+	// http://localhost:8080/home/index
+	@RequestMapping(value = "/home/index")
+	public String index(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
 		model.addAttribute("name", name);
-        return "home/index";
-    }
-//	
-//	@RequestMapping(value="/index", method=RequestMethod.POST)
-//    public String allowAccessToLoginPagePost() 
-//    {
-//        return "Home page *Sell Your Own Home* Post method";
-//    }
-	
+		return "home/index";
+	}
+
 	@RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-}
+	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+	
+//	@RequestMapping(value="/index", method=RequestMethod.GET)
+//  public String allowAccessToLoginPagePost() 
+//  {
+//      return "Home page *Sell Your Own Home* Get method";
+//  }
+	
+//	@RequestMapping(value="/index", method=RequestMethod.POST)
+//  public String allowAccessToLoginPagePost() 
+//  {
+//      return "Home page *Sell Your Own Home* Post method";
+//  }
 
 }
