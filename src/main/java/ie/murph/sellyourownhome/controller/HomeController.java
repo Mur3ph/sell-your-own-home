9ead0af6e696390c2feb2da2e71156b2ed97affd
Mur3ph <paulmurphymainstreet@gmail.com>
2017-08-05 22:39:36 +0100

    controller to view

--- src/main/java/ie/murph/sellyourownhome/controller/HomeController.java
+++ src/main/java/ie/murph/sellyourownhome/controller/HomeController.java
@@ -1,19 +1,19 @@
 package ie.murph.sellyourownhome.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RestController
-@EnableAutoConfiguration
 public class HomeController {
 	
-//	http://localhost:8080/home
-	@RequestMapping("/home")
-    String greetingsFromHomepage() {
-        return "Home page *Sell Your Own Home*";
-    }
+//	http://localhost:8080/home/index
+	
+	@RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+	}
 
 }
