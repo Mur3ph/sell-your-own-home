0adc3b0c7d21c7f8f73a97676f21ed06837028bb
Mur3ph <paulmurphymainstreet@gmail.com>
2017-08-16 22:05:28 +0100

    Controller annotation and first method

--- src/main/java/ie/murph/sellyourownhome/controller/ViagerController.java
+++ src/main/java/ie/murph/sellyourownhome/controller/ViagerController.java
@@ -1,5 +1,14 @@
 package ie.murph.sellyourownhome.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ViagerController {
 
+	// http://localhost:8080/viager/index
+	@RequestMapping("viager/index")
+	public String viager() {
+		return "viager/index";
+	}
 }
