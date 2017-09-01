d1f80ef1227489c0226cfa727f8c03aed62a2fa2
Mur3ph <paulmurphymainstreet@gmail.com>
2017-09-01 22:46:49 +0100

    Alter sql, create new table for images and add constraints

--- src/main/resources/static/sql/create-table-statement.sql
+++ src/main/resources/static/sql/create-table-statement.sql
@@ -1,5 +1,3 @@
--- Script to create database for sell your own house website
-
 CREATE TABLE Users
 (
   user_id                                  INTEGER     NOT NULL,
@@ -10,7 +8,7 @@ CREATE TABLE Users
   city                                     VARCHAR(50) NOT NULL,
   date_of_birth                            DATE        NOT NULL,
   gender                                   VARCHAR(50) NOT NULL,
-  phone                                    VARCHAR(50) NOT NULL,
+  phone                                    VARCHAR(20) NOT NULL,
   email                                    VARCHAR(50) NOT NULL,
   CONSTRAINT pk_user                       PRIMARY KEY (user_id),
   CONSTRAINT ck_date_of_birth              CHECK (date_of_birth < SYSDATE),
@@ -20,17 +18,19 @@ CREATE TABLE Users
 
 CREATE TABLE Buyer
 (
-  buyer_id                               INTEGER     NOT NULL,
-  user_id                                INTEGER     NOT NULL,
-  property_type                          VARCHAR(50) NOT NULL,
-  country                                VARCHAR(50) NOT NULL,
-  city                                   VARCHAR(50) NOT NULL,
-  min_area                               VARCHAR(50) NOT NULL,
-  max_area                               VARCHAR(50) NOT NULL,
-  min_budget                             VARCHAR(50) NOT NULL,
-  max_budget                             VARCHAR(50) NOT NULL,
+  buyer_id                               INTEGER        NOT NULL,
+  user_id                                INTEGER        NOT NULL,
+  property_type                          VARCHAR(50)    NOT NULL,
+  country                                VARCHAR(50)    NOT NULL,
+  city                                   VARCHAR(50)    NOT NULL,
+  min_area                               NUMBER(10, 2)  NOT NULL,
+  max_area                               NUMBER(10, 2)  NOT NULL,
+  min_budget                             NUMBER(10, 2)  NOT NULL,
+  max_budget                             NUMBER(10, 2)  NOT NULL,
   CONSTRAINT pk_buyer                    PRIMARY KEY (buyer_id),
-  CONSTRAINT fk_buyer_user               FOREIGN KEY (user_id) REFERENCES Users (user_id)
+  CONSTRAINT fk_buyer_user               FOREIGN KEY (user_id) REFERENCES Users (user_id),
+  CONSTRAINT ck_max_min_budget           CHECK ( min_budget <= max_budget ),
+  CONSTRAINT ck_max_min_area             CHECK ( min_area <= max_area )
 );
 
 CREATE TABLE Pauly_Admin
@@ -46,15 +46,15 @@ CREATE TABLE Pauly_Admin
 -- TODO: property_address_id column might actually meant to be property_advertizment_id
 CREATE TABLE Property
 (
-  property_id                              INTEGER     NOT NULL,
-  property_type                            VARCHAR(50) NOT NULL,
-  property_address_id                      INTEGER     NOT NULL,
-  city                                     VARCHAR(50) NOT NULL,
-  country                                  VARCHAR(50) NOT NULL,
-  property_date                            DATE        NOT NULL,
-  number_of_rooms                          INTEGER     NOT NULL,
-  area_size                                INTEGER     NOT NULL,
-  per_unit_price                           INTEGER     NOT NULL,
+  property_id                              INTEGER          NOT NULL,
+  property_type                            VARCHAR(50)      NOT NULL,
+  property_ad_id                           INTEGER          NOT NULL,
+  city                                     VARCHAR(50)      NOT NULL,
+  country                                  VARCHAR(50)      NOT NULL,
+  property_date                            DATE             NOT NULL,
+  number_of_rooms                          INTEGER          NOT NULL,
+  area_size                                INTEGER          NOT NULL,
+  per_unit_price                           NUMBER(10, 2)    NOT NULL,
   CONSTRAINT pk_property                   PRIMARY KEY (property_id)
 );
 
@@ -74,19 +74,29 @@ CREATE TABLE Seller
 
 CREATE TABLE Room
 (
-  room_id                                  INTEGER     NOT NULL,
-  property_id                              INTEGER     NOT NULL,
-  room_name                                VARCHAR(50) NOT NULL,
-  area                                     VARCHAR(50) NOT NULL,
-  image                                    VARCHAR(50) NOT NULL,
+  room_id                                  INTEGER          NOT NULL,
+  property_id                              INTEGER          NOT NULL,
+  room_name                                VARCHAR(50)      NOT NULL,
+  area                                     NUMBER(20, 2)    NOT NULL,
   CONSTRAINT pk_room                       PRIMARY KEY (room_id),
   CONSTRAINT fk_room_prop                  FOREIGN KEY (property_id) REFERENCES Property (property_id)
 );
 
+CREATE TABLE Image
+(
+  image_id                  INTEGER         NOT NULL,
+  room_id                   INTEGER         NOT NULL,
+  image_name                VARCHAR2(30),
+  mime_type                 VARCHAR2(30),
+  CONTENT                   BLOB,
+  CONSTRAINT pk_image       PRIMARY KEY (image_id),
+  CONSTRAINT fk_image_room  FOREIGN KEY (room_id) REFERENCES Room (room_id)
+)
+
 
 CREATE TABLE Address
 (
-  property_address_id                      INTEGER     NOT NULL,
+  address_id                               INTEGER     NOT NULL,
   city                                     VARCHAR(50) NOT NULL,
   property_id                              INTEGER     NOT NULL,
   plot_number                              VARCHAR(50) NOT NULL,
@@ -106,9 +116,7 @@ CREATE TABLE Feature
   feature_id                               INTEGER     NOT NULL,
   property_id                              INTEGER     NOT NULL,
   feature_name                             VARCHAR(50) NOT NULL,
-  description                              VARCHAR(50) NOT NULL,
+  feature_description                      VARCHAR(50) NOT NULL,
   CONSTRAINT pk_feature                    PRIMARY KEY (feature_id),
   CONSTRAINT fk_feature_prop               FOREIGN KEY (property_id) REFERENCES Property (property_id)
 );
-
-
--- src/main/resources/static/sql/drop-table-statement.sql
+++ src/main/resources/static/sql/drop-table-statement.sql
@@ -1,6 +1,7 @@
 DROP TABLE Seller;
 DROP TABLE Buyer;
 DROP TABLE Pauly_Admin;
+DROP TABLE Image;
 DROP TABLE Room;
 DROP TABLE Address;
 DROP TABLE Feature;
