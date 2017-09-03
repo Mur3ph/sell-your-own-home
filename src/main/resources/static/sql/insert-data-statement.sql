83527bedde12f4c1777bf7f58f2578d96f608fba
Mur3ph <paulmurphymainstreet@gmail.com>
2017-09-03 20:57:25 +0100

    insert data file

---
+++ src/main/resources/static/sql/insert-data-statement.sql
@@ -0,0 +1,12 @@
+DROP TABLE Seller;
+DROP TABLE Buyer;
+DROP TABLE Pauly_Admin;
+DROP TABLE Image;
+DROP TABLE Room;
+DROP TABLE Address;
+DROP TABLE Feature;
+DROP TABLE Property;
+DROP TABLE Users;
+
+
+
