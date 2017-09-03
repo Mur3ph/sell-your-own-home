671c0d0593f1c6bb9b20128fadbf5983ea8cbd3f
Mur3ph <paulmurphymainstreet@gmail.com>
2017-09-03 21:13:46 +0100

    added/inserted data to tables

--- src/main/resources/static/sql/insert-data-statement.sql
+++ src/main/resources/static/sql/insert-data-statement.sql
@@ -1,12 +1,19 @@
-DROP TABLE Seller;
-DROP TABLE Buyer;
-DROP TABLE Pauly_Admin;
-DROP TABLE Image;
-DROP TABLE Room;
-DROP TABLE Address;
-DROP TABLE Feature;
-DROP TABLE Property;
-DROP TABLE Users;
+
+INSERT INTO Users VALUES (1, 'buyer', 'Paul', 'Murphy', 'Ireland', DATE, 'male', '087 123 4567', 'email@gmail.com');
+INSERT INTO Buyer VALUES (1, 1, 'house', 'Ireland', 'Dublin', 100, 1000, 1000.00, 100000.00);
+INSERT INTO Pauly_Admin VALUES (1, 1, 'username', 'password');
+
+INSERT INTO Property VALUES (1, 'house', 101, 'Duleek city', 'Ireland', DATE, 3, 1000, 250);
+
+INSERT INTO Seller VALUES (1, 1, 101, 'Me');
+
+INSERT INTO Room VALUES (1, 1, 'Bed room', 100);
+
+INSERT INTO Image VALUES (1, 1, 'image name', 'mime_type', 'BLOB');
+
+INSERT INTO Address VALUES (1, 1, 'plot number 1', 'property name yoda', 'floor number 3', 'Area name mars', 'Duleek City', 'Meath County', 'Ireland', 'EIRE zip');
 
 
+INSERT INTO Feature VALUES ( 1, 1, 'feature name', 'feature description');
 
+                                          
