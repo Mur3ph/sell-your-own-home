9761ce838db1e1043369946325a74b8765ebd994
Mur3ph <paulmurphymainstreet@gmail.com>
2018-07-14 23:04:30 +0100

    Implemented gui, controller and bootstrapped login page

---
+++ src/main/java/ie/murph/sellyourownhome/controller/LoginController.java
@@ -0,0 +1,15 @@
+package ie.murph.sellyourownhome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	// http://localhost:8080/login/index
+	@RequestMapping(value = "/login")
+	public String index() {
+		return "login/index";
+	}
+
+}
