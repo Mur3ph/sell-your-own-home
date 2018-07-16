03e8e2a038e948dd4dc8a644b4f6d343df610790
Mur3ph <paulmurphymainstreet@gmail.com>
2018-07-16 21:16:15 +0100

    added logging to project

--- src/main/java/ie/murph/sellyourownhome/controller/LoginController.java
+++ src/main/java/ie/murph/sellyourownhome/controller/LoginController.java
@@ -1,15 +1,21 @@
 package ie.murph.sellyourownhome.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+	private static final String urlLoginPage = "login/index";
 		
 	// http://localhost:8080/login/index
-	@RequestMapping(value = "login/index")
-	public String index() {
-		return "login/index";
+	@RequestMapping(value = urlLoginPage)
+	public String routeToLoginPage() {
+		LOGGER.info("++routeToLoginPage()");
+		return urlLoginPage;
 	}
 
 }
