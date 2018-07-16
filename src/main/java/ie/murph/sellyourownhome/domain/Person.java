65d14bf15351a75961530d5b1eef11f87c4609f8
Mur3ph <paulmurphymainstreet@gmail.com>
2018-07-16 22:18:30 +0100

    domain object

--- src/main/java/ie/murph/sellyourownhome/domain/Person.java
+++ src/main/java/ie/murph/sellyourownhome/domain/Person.java
@@ -1,5 +1,36 @@
 package ie.murph.sellyourownhome.domain;
 
+
+
 public class Person {
+
+	private Long personId;
+
+	private String firstName;
+	private String lastName;
+
+	public Long getPersonId() {
+		return personId;
+	}
+
+	public void setPersonId(Long personId) {
+		this.personId = personId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
 }
